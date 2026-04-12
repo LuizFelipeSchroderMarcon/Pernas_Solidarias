8f36e9612e68c6ef08e8264353cfdc813501b910
Madu3304 <mdua0722@gmail.com>
2026-04-12 19:21:32 -0300

    Ajustes- sugestao

---
+++ main.js
@@ -0,0 +1,7 @@
+// #Data: 10/04/2026 - Maria Eduarda
+
+//Observação: 
+// verificar o ponto 1.2-Origem da Demanda e Evidências
+//
+// topico: Pesquisa com Usuários, não foi preenchido. 
+// topic: Número de pessoas entrevistadas, pode ser adicionado uma segunda pessoal, que foi o seu senhor Jose carlos. 
