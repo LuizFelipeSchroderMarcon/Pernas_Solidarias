91f53600ff2e9fa84131130ecedf3023c11edcf9
Madu3304 <mdua0722@gmail.com>
2026-04-12 19:38:04 -0300

    observacao 2

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 // #Data: 10/04/2026 - Maria Eduarda
 
-//Observação: 
+//Observação 01: 
 // verificar o ponto 1.2-Origem da Demanda e Evidências
 //
 // topico: Pesquisa com Usuários, não foi preenchido. 
 // topic: Número de pessoas entrevistadas, pode ser adicionado uma segunda pessoal, que foi o seu senhor Jose carlos. 
+
+//Observação 02: 
+// Adicionar o texto a seguir no inicio do documento. 
+// % -------------------------------------------------
+// % IDENTIFICAÇÃO
+// % -------------------------------------------------
+// \section*{Identificação}
+// \addcontentsline{toc}{section}{Identificação}
+
+// \begin{itemize}
+//     \item \textbf{Título do Projeto:} Software Brava
+//     \item \textbf{Linha de Projeto:} Aplicativo.
+//     \item \textbf{Autor:} Maria Eduarda Nunes
+//     \item \textbf{Data da Proposta:} 10/03/2026
+//     \item \textbf{Versão:} 1.0
+// \end{itemize}
+
+// % -------------------------------------------------
